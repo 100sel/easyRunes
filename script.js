6b15d4cc053fc71061c1622bb6eeadc2d57a00ca
100sel <romain.peturaud@gmail.com>
2022-05-08 02:19:28 +0200

    done

--- script.js
+++ script.js
@@ -3,21 +3,34 @@ const statsTypeData = document.querySelectorAll(".userTypeData");
 const getOptiButton = document.querySelector("#getOptiButton");
 const displayOpti = document.querySelector("#displayOpti");
 
-//runeDamage
-let statsValue = [10, 21, 7, 5];
-let statsType = ['speed', 'attackPercent', 'criDamagePercent', 'criRate'];
-//runeBalanced
-//let statsValue = [10, 14, 14, 7];
-//let statsType = ['speed', 'attackPercent', 'hpPercent', 'accuracy'];
-//runeSupport
-//let statsValue = [10, 21, 7, 7];
-//let statsType = ['speed', 'hpPercent', 'defensePercent', 'accuracy'];
-//runeNuker
-//let statsValue = [10, 7, 10, 14];
-//let statsType = ['speed', 'attackPercent', 'criRate', 'criDamage'];
+const testDmgBtn = document.querySelector("#getOptiButtonDmg");
+const testBlcBtn = document.querySelector("#getOptiButtonBlc");
+const testSupBtn = document.querySelector("#getOptiButtonSup");
+const testNkrBtn = document.querySelector("#getOptiButtonNkr");
 
-//let statsValue = [ ];
-//let statsType = [ ];
+let statsValue = [];
+let statsType = [];
+
+function testDmg() {
+    statsValue = [10, 21, 7, 5];
+    statsType = ['speed', 'attackPercent', 'criDamagePercent', 'criRate'];
+}
+
+function testBlc() {
+    statsValue = [10, 14, 14, 7];
+    statsType = ['speed', 'attackPercent', 'hpPercent', 'accuracy'];
+}
+
+function testSup() {
+    statsValue = [10, 21, 7, 7];
+    statsType = ['speed', 'hpPercent', 'defensePercent', 'accuracy'];
+}
+
+function testNkr() {
+    statsValue = [5, 7, 15, 14];
+    statsType = ['speed', 'attackPercent', 'criRate', 'criDamagePercent'];
+}
+
 let Rune = { };
 
 function Stat(value, type) {
@@ -46,13 +59,13 @@ const coeffsTemplateDamage = [
 const templateDamage = new Template('templateDamage', coeffsTemplateDamage);
 
 const coeffsTemplateBalanced = [
-    new Stat(1.3, 'speed'),
-    new Stat(1, 'hpPercent'),
-    new Stat(1, 'defensePercent'), 
-    new Stat(1, 'attackPercent'),    
+    new Stat(1.1, 'speed'),
+    new Stat(1.2, 'hpPercent'),
+    new Stat(1.2, 'defensePercent'), 
+    new Stat(1.2, 'attackPercent'),    
     new Stat(0.7, 'criRate'),
     new Stat(0.7, 'criDamagePercent'),
-    new Stat(0.8, 'accuracyPercent'),
+    new Stat(1, 'accuracyPercent'),
     new Stat(1, 'resistance'),
     new Stat(1, 'hpFlat'),
     new Stat(1, 'attackFlat'),
@@ -61,27 +74,26 @@ const coeffsTemplateBalanced = [
 const templateBalanced = new Template('templateBalanced', coeffsTemplateBalanced);
 
 const coeffsTemplateSupport = [
-    new Stat(1.1, 'speed'),
+    new Stat(1.2, 'speed'),
     new Stat(1.3, 'hpPercent'),
-    new Stat(1.2, 'defensePercent'), 
+    new Stat(1.3, 'defensePercent'), 
     new Stat(0.7, 'attackPercent'),    
     new Stat(0.7, 'criRate'),
     new Stat(0.7, 'criDamagePercent'),
-    new Stat(1, 'accuracyPercent'),
+    new Stat(1.2, 'accuracyPercent'),
     new Stat(1, 'resistance'),
     new Stat(1.05, 'hpFlat'),
     new Stat(0.95, 'attackFlat'),
     new Stat(1.05, 'defenseFlat')
 ];
-
 const templateSupport = new Template('templateSupport', coeffsTemplateSupport);
 
 const coeffsTemplateNuker = [
-    new Stat(1.1, 'speed'),
+    new Stat(1, 'speed'),
     new Stat(0.7, 'hpPercent'),
     new Stat(0.7, 'defensePercent'), 
     new Stat(1.1, 'attackPercent'),    
-    new Stat(1.2, 'criRate'),
+    new Stat(1.3, 'criRate'),
     new Stat(1.2, 'criDamagePercent'),
     new Stat(0.8, 'accuracyPercent'),
     new Stat(1, 'resistance'),
@@ -89,8 +101,22 @@ const coeffsTemplateNuker = [
     new Stat(1.05, 'attackFlat'),
     new Stat(1, 'defenseFlat')
 ];
-
 const templateNuker = new Template('templateNuker', coeffsTemplateNuker);
+
+const coeffsTemplateNeutral = [
+    new Stat(1, 'speed'),
+    new Stat(1, 'hpPercent'),
+    new Stat(1, 'defensePercent'), 
+    new Stat(1, 'attackPercent'),    
+    new Stat(1, 'criRate'),
+    new Stat(1, 'criDamagePercent'),
+    new Stat(1, 'accuracyPercent'),
+    new Stat(1, 'resistance'),
+    new Stat(1, 'hpFlat'),
+    new Stat(1, 'attackFlat'),
+    new Stat(1, 'defenseFlat')
+];
+const templateNeutral = new Template('templateNeutral', coeffsTemplateNeutral);
 
 function getStats() {
     statsValueData.forEach(e => {statsValue.push(parseInt(e.value, 10))});
@@ -107,20 +133,20 @@ function setRune(rune, value, type) {
 
 function hitsCalc(type) { 
     if (type.match(/Percent/)) {
-        return 7;
+        return 8;
     } 
     if (type.match(/Flat/)) {
-        return 500;
+        return 0;
     } 
     if (type.match == 'resistance') {
-        return 14;
+        return 12;
     }
-    return 5;
+    return 6;
 }
 
 function setHits(rune) {
     rune.stats.forEach(stat => {
-        stat.hits = Math.round((stat.value / hitsCalc(stat.type) * 100) / 10);
+        stat.hits = stat.value / hitsCalc(stat.type);
     })
 }
 
@@ -134,7 +160,7 @@ function templateCalc(runeStatsType, templateCoeffs) {
     return coeffValue;
 }
 
-function setTemplate(rune, ...template) {
+function setOpti(rune, ...template) {
     let opti = [ ];
     template.forEach(template => {
         let totalHits = 0;
@@ -152,16 +178,33 @@ function setTemplate(rune, ...template) {
                 opti.push(new Stat(totalHits, 'Support'));
                 break;
             case 'templateNuker':
-                opti.push(new Stat(totalHits, 'Nuker'));                
+                opti.push(new Stat(totalHits, 'Nuker'));
+                break;  
+            case 'templateNeutral':
+                opti.push(new Stat(totalHits, 'Neutral'));
+                break;             
         }
-        rune.efficiency = opti; 
+        rune.opti = opti; 
     })
 }
 
+function getEfficiency(rune) {
+    let efficiency = [ ];
+    rune.opti.forEach(opti => {
+        efficiency.push(new Stat((100 - ((9 - opti.value)*7)).toFixed(2), opti.type));
+    })
+    let totalHits = 0;
+    rune.stats.forEach(nonOpti => {
+        totalHits += nonOpti.hits;
+    })
+    rune.efficiency = efficiency;
+    rune.nonOpti = totalHits;
+}
+
 function showOpti() {
-    Rune.efficiency.forEach(opti => {
+    Rune.efficiency.forEach(effic => {
         let runeOpti = document.createElement("div");
-        runeOpti.innerHTML = opti.type + ' : ' + opti.value.toFixed(2);
+        runeOpti.innerHTML = effic.type + ' : ' + effic.value;
         displayOpti.appendChild(runeOpti);
     });
 }
@@ -170,8 +213,14 @@ function getOpti() {
     //getStats();
     setRune(Rune, statsValue, statsType);
     setHits(Rune);
-    setTemplate(Rune, templateDamage, templateBalanced, templateSupport, templateNuker);
+    setOpti(Rune, templateDamage, templateBalanced, templateSupport, templateNuker, templateNeutral);
+    getEfficiency(Rune);
+    console.log(Rune);
     showOpti();
 }
 
 getOptiButton.onclick = getOpti;
+testDmgBtn.onclick = testDmg;
+testBlcBtn.onclick = testBlc;
+testSupBtn.onclick = testSup;
+testNkrBtn.onclick = testNkr;
